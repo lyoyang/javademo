ef15ad22740ad2f460ddb7ccabe8a4a946baf019
yangbing <lyoyang@123.com>
2018-10-31 23:58:19 +0800

    netty demo

---
+++ src/main/java/com/lyoyang/netty/EchoClient.java
@@ -0,0 +1,48 @@
+package com.lyoyang.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.net.InetSocketAddress;
+
+public class EchoClient {
+
+    private String host;
+    private int port;
+
+    public EchoClient() {
+    }
+
+    public EchoClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void start() throws Exception {
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group).channel(NioSocketChannel.class).remoteAddress(new InetSocketAddress(host, port))
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel channel) throws Exception {
+                            channel.pipeline().addLast(new EchoClientHandler());
+                        }
+                    });
+            ChannelFuture sync = bootstrap.connect().sync();
+            sync.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        EchoClient echoClient = new EchoClient("localhost", 8080);
+        echoClient.start();
+
+    }
+}
