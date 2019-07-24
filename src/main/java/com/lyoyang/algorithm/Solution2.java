0535dfe62d7cb5a36d9aca29bddd56e31f9b4b9d
yangbing <yangbing@ipaynow.cn>
2019-07-24 18:40:03 +0800

    optimize code

---
+++ src/main/java/com/lyoyang/algorithm/Solution2.java
@@ -0,0 +1,34 @@
+package com.lyoyang.algorithm;
+
+/**
+ * @author: yangbing
+ * @Date: 2019/7/24 17:41
+ * @Description:
+ * 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+ * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution2 {
+
+
+    public static int method1(int[] nums) {
+        int tmp = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (tmp == nums[i]) {
+                //todo
+            }
+        }
+        return nums.length;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+
+}
