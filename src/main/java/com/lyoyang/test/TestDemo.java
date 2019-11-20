673d05a1d94af7d8175049f63c161bf160fedfbc
yangbing <yangbing@ipaynow.cn>
2019-11-20 10:10:03 +0800

    'test'

--- src/main/java/com/lyoyang/test/TestDemo.java
+++ src/main/java/com/lyoyang/test/TestDemo.java
@@ -1,6 +1,7 @@
 package com.lyoyang.test;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.google.common.io.Files;
@@ -152,5 +153,15 @@ public class TestDemo {
 //            });
 //        }
 //        pool.invokeAll(callables);
+
+//        int i;
+//        for (i = 0; i < 10; i++) {
+//            System.out.println("hello-->" + i);
+//        }
+//        System.out.println(i);
+
+//        String certainDate = DateUtil.getCertainDate(-2);
+//        String settleDate = DateUtil.getCertainDate(certainDate, 1);
+//        System.out.println(certainDate + "---->" + settleDate);
     }
 }
