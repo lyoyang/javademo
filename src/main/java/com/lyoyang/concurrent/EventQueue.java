cb71cca6e313cc2234425865bec4e4baba61b509
杨兵 <yangbing@ipaynow.cn>
2020-02-20 19:16:30 +0800

    thread

--- src/main/java/com/lyoyang/concurrent/EventQueue.java
+++ src/main/java/com/lyoyang/concurrent/EventQueue.java
@@ -2,20 +2,21 @@ package com.lyoyang.concurrent;
 
 import java.util.LinkedList;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static java.lang.Thread.currentThread;
 
 public class EventQueue {
 
-    static class Event {}
+    private static final AtomicInteger atomicInteger = new AtomicInteger(1);
 
+    static class Event {}
 
     private final int max;
 
-
     private final static int DEFAULT_MAX = 10;
 
-    private final LinkedList<Event> linkedList = new LinkedList<>();
+    private final LinkedList<Integer> linkedList = new LinkedList<>();
 
     public EventQueue() {
         this(DEFAULT_MAX);
@@ -27,7 +28,7 @@ public class EventQueue {
 
     public void offer(Event event) {
         synchronized (linkedList) {
-            if (linkedList.size() >= max) {
+            while (linkedList.size() >= max) {
                 try {
                     console("the queue is full");
                     linkedList.wait();
@@ -35,20 +36,16 @@ public class EventQueue {
                     e.printStackTrace();
                 }
             }
-            System.out.println();
-            linkedList.addLast(event);
-            linkedList.notify();
-            console("the new event is submitted.");
+            int andIncrement = atomicInteger.getAndIncrement();
+            linkedList.addLast(andIncrement);
+            linkedList.notifyAll();
+            console("the new event [" + andIncrement+ "] offered，size=" + linkedList.size());
         }
-
-
-
-
     }
 
     public void take() {
         synchronized (linkedList) {
-            if (linkedList.size() <= 0) {
+            while (linkedList.size() <= 0) {
                 try {
                     console("the queue is empty");
                     linkedList.wait();
@@ -56,9 +53,9 @@ public class EventQueue {
                     e.printStackTrace();
                 }
             }
-            Event event = linkedList.removeFirst();
-            linkedList.notify();
-            console("the event " + event +"is submitted.");
+            Integer data = linkedList.removeFirst();
+            linkedList.notifyAll();
+            console("the event [" + data + "] is taked,size=" + linkedList.size());
         }
     }
 
@@ -75,9 +72,24 @@ public class EventQueue {
             for (;;) {
                 eventQueue.offer(new EventQueue.Event());
             }
-        }, "Producer").start();
+        }, "Producer1").start();
+        new Thread(() -> {
+            for (;;) {
+                eventQueue.offer(new EventQueue.Event());
+            }
+        }, "Producer2").start();
 
 
+        new Thread(() -> {
+            for (;;) {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                eventQueue.take();
+            }
+        }, "Consumer1").start();
 
         new Thread(() -> {
             for (;;) {
@@ -88,7 +100,17 @@ public class EventQueue {
                 }
                 eventQueue.take();
             }
-        }, "Consumer").start();
+        }, "Consumer2").start();
+        new Thread(() -> {
+            for (;;) {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                eventQueue.take();
+            }
+        }, "Consumer3").start();
     }
 
 
