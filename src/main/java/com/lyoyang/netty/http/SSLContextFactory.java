e1776348fdb700db250389c8a7e9f973c54de109
杨兵 <yangbing@ipaynow.cn>
2020-07-07 19:35:58 +0800

    ssl

--- src/main/java/com/lyoyang/netty/http/SSLContextFactory.java
+++ src/main/java/com/lyoyang/netty/http/SSLContextFactory.java
@@ -3,10 +3,6 @@ package com.lyoyang.netty.http;
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import java.io.InputStream;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.security.KeyStore;
 
 public class SSLContextFactory {
@@ -18,7 +14,6 @@ public class SSLContextFactory {
         InputStream resourceAsStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("cert/sChat.jks");
         keyStore.load(resourceAsStream, filePass);
         KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-//        KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
         kmf.init(keyStore, filePass);
         sslContext.init(kmf.getKeyManagers(), null, null);
         return sslContext;
